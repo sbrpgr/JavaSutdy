44302daf71c3ab45cc7935f9290121bcc75486ee
sbrpgr <sprbxr@gmail.com>
2022-12-10 23:20:39 +0900

    "1210"

---
+++ src/bj06/Main.java
@@ -0,0 +1,29 @@
+package bj06;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int ns = 0; ns < n; ns++) {
+			
+		
+		int r = sc.nextInt();
+		String s = sc.next();
+		String[] sa;
+		sa = s.split("");
+		
+		for (int i = 0; i < s.length(); i++) {
+			for(int j=0; j <r; j++) {
+				System.out.print(sa[i]);
+			}
+		}
+		}
+		
+	}
+
+}
